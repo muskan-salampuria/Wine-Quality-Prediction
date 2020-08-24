b0c6defd2b76ad764dd3213970a5f7a7c521292c
Muskan Salampuria <muskan.salampuria.fiem.cse17@teamfuture.in>
2020-08-25 00:04:21 +0530

    Update wine_quality_prediction_using_naive_bayes.py

--- wine_quality_prediction_using_naive_bayes.py
+++ wine_quality_prediction_using_naive_bayes.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[17]:
-
+                                                                         # In[17]:
 
 import pandas as pd
 
@@ -12,7 +11,7 @@ wine=load_wine()
 dir(wine)
 
 
-# In[18]:
+                                                                        # In[18]:
 
 
 #Data Frame is created from Wine Dataset
@@ -20,14 +19,14 @@ df=pd.DataFrame(wine.data,columns=wine.feature_names)
 df.head()
 
 
-# In[19]:
+                                                                        # In[19]:
 
 
 #target column
 target=wine.target
 
 
-# In[20]:
+                                                                        # In[20]:
 
 
 #splitting test and train datasets
@@ -35,7 +34,7 @@ from sklearn.model_selection import train_test_split
 X_train,X_test, y_train,y_test=train_test_split(df,target,test_size=0.2)
 
 
-# In[21]:
+                                                                        # In[21]:
 
 
 #model is for Multinomial Naive Bayes
@@ -43,7 +42,7 @@ from sklearn.naive_bayes import MultinomialNB
 model=MultinomialNB()
 
 
-# In[22]:
+                                                                        # In[22]:
 
 
 #model1 is for Gaussian Naive Bayes
@@ -51,49 +50,49 @@ from sklearn.naive_bayes import GaussianNB
 model1=GaussianNB()
 
 
-# In[23]:
+                                                                        # In[23]:
 
 
 #Multinomial model is trained
 model.fit(X_train,y_train)
 
 
-# In[24]:
+                                                                        # In[24]:
 
 
 #Gaussian Model is trained
 model1.fit(X_train,y_train)
 
 
-# In[25]:
+                                                                        # In[25]:
 
 
 #Score of Multinomial model
 model.score(X_test,y_test)
 
 
-# In[26]:
+                                                                        # In[26]:
 
 
 #score of Gaussian model
 model1.score(X_test,y_test)
 
 
-# In[27]:
+                                                                        # In[27]:
 
 
 #Predicting Values using Multinomial model
 model.predict(X_test[:10])
 
 
-# In[28]:
+                                                                        # In[28]:
 
 
 #Values from the dataset
 y_test[:10]
 
 
-# In[29]:
+                                                                        # In[29]:
 
 
 #Storing all the predicted values for X_test in y_predicted using Multinomial model
@@ -104,7 +103,7 @@ from sklearn.metrics import confusion_matrix
 cm=confusion_matrix(y_test, y_predicted)
 
 
-# In[30]:
+                                                                        # In[30]:
 
 
 #Plotting confusion matrix
@@ -117,7 +116,7 @@ plt.xlabel('Predicted')
 plt.ylabel('Truth')
 
 
-# In[31]:
+                                                                        # In[31]:
 
 
 #CONCLUSION: Gaussian Model is Better than Multinomial model in this dataset. 
